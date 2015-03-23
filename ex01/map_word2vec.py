fab28b50bd66306e6d0ebe2ac429636ed64586d6
gw0 [http://gw.tnode.com/] <gw.2014@tnode.com>
2015-03-23 14:51:23 +0100

    Improve vocabulary strip helpers.

--- ex01/map_word2vec.py
+++ ex01/map_word2vec.py
@@ -28,49 +28,38 @@ log = common.logging.getLogger(__name__)
 
 ### Vocabulary mapping
 
-def strip_phrase(text, _pats=None):
-    """Strip phrase to basic lowercase alphanumeric characters."""
-    if _pats is None:
-        _pats = [
-            (re.compile("[^a-z0-9#\\.,;/%$]"), ""),
-            (re.compile("[0-9]+"), "#"),
-            #(re.compile("[0-9][0-9]"), "##"),
-            #(re.compile("##[0-9]"), "###"),
-        ]
-
-    strip = text.lower()
-    for pat, repl in _pats:
-        strip = pat.sub(repl, strip)
-    return strip
-
-
 @common.profile
-def map_strip_base(vocab_base, get_count=None):
-    """Build mapping of stripped helper vocabulary to base vocabulary."""
+def map_strips_base(strip_helpers, base_vocab, get_count=None):
+    """Build mappings of stripped helper vocabularies to base vocabulary."""
     if get_count is None:
         get_count = lambda vocab, token: vocab[token].count
 
-    vocab_strip = {}
-    for token in vocab_base:
-        strip = strip_phrase(token)
-        if strip not in vocab_strip:
-            vocab_strip[strip] = token
-        else:
-            cnt_cur = get_count(vocab_base, vocab_strip[strip])
-            cnt_new = get_count(vocab_base, token)
-            if cnt_new > cnt_cur:
-                vocab_strip[strip] = token
-    return vocab_strip
+    strip_vocabs = [ {}  for _ in strip_helpers ]
+    for token in base_vocab:
+        strip = token
+        for strip_func, strip_vocab in zip(strip_helpers, strip_vocabs):
+            strip = strip_func(strip)  # strip phrase
+
+            if strip not in strip_vocab:  # new mapping
+                strip_vocab[strip] = token
+            else:  # duplicate mapping
+                cnt_cur = get_count(base_vocab, strip_vocab[strip])
+                cnt_new = get_count(base_vocab, token)
+                if cnt_new > cnt_cur:
+                    strip_vocab[strip] = token
+    return strip_vocabs
 
 @common.profile
-def map_sent_base(sentences, vocab_base, vocab_strip=None, longest=True, max_len=5, delimiter="_"):
+def map_sent_base(sentences, base_vocab, strip_helpers=None, strip_vocabs=None, longest=True, max_len=5, delimiter="_"):
     """Build mapping of words/phrases in sentences to base vocabulary."""
-    if vocab_strip is None:
-        vocab_strip = {}
+    if strip_helpers is None:
+        strip_helpers = []
+    if strip_vocabs is None:
+        strip_vocabs = []
 
-    pat = re.compile("[^a-z0-9#\\.,;/%$]")
+    pat = re.compile("[^A-Za-z0-9#%$]")
 
-    vocab_sent = {}
+    sent_vocab = {}
     missing = {}
     cnt = 0
     for sentence in sentences:
@@ -83,37 +72,47 @@ def map_sent_base(sentences, vocab_base, vocab_strip=None, longest=True, max_len
                 # already matched
                 text_list = [ sentence[k]['Text']  for k in range(i, j) ]
                 text = delimiter.join(text_list)
-                if text in vocab_sent:
+                if text in sent_vocab:
                     break
 
                 # direct match
-                if text in vocab_base:
-                    vocab_sent[text] = text
+                if text in base_vocab:
+                    sent_vocab[text] = text
                     if longest:
                         break
                     else:
                         continue
 
                 # phrase check
-                if pat.sub("", text_list[0]) == "" or pat.sub("", text_list[-1]) == "":
+                text_part = None
+                for text_part in text_list:
+                    if pat.sub("", text_part) == "":
+                        text_part = None
+                        break
+                if text_part is None:
                     continue
 
-                # lower case, strip non-alphanumeric, strip numbers
-                strip = strip_phrase(text)
-                if strip in vocab_strip:
-                    vocab_sent[text] = vocab_strip[strip]
-                    if longest:
+                # stripped matching
+                strip_vocab = {}
+                strip = text
+                for strip_func, strip_vocab in zip(strip_helpers, strip_vocabs):
+                    strip = strip_func(strip)  # strip phrase
+                    if strip in strip_vocab:
+                        sent_vocab[text] = strip_vocab[strip]
                         break
+                if strip in strip_vocab:
+                    if longest:
+                       break
                     else:
                         continue
 
-            if text not in vocab_sent:
+            if text not in sent_vocab:
                 try:
                     missing[text] += 1
                 except KeyError:
                     missing[text] = 1
                     log.debug("- not in word2vec: {}".format(text))
-    return vocab_sent, missing, cnt
+    return sent_vocab, missing, cnt
 
 ### PDTB parses
 
@@ -162,12 +161,12 @@ def load_word2vec(word2vec_bin):
     return model
 
 @common.profile
-def map_base_word2vec(vocab_base, model):
+def map_base_word2vec(base_vocab, model):
     """Build mapping of vocabulary to word2vec vectors."""
 
     map_word2vec = {}
     cnt = 0
-    for text, token in vocab_base.iteritems():
+    for text, token in base_vocab.iteritems():
         if cnt % 50000 == 0:
             log.debug("- processing at {}...".format(cnt))
         cnt += 1
@@ -184,11 +183,25 @@ def build(word2vec_bin, parses_json):
     model = load_word2vec(word2vec_bin)
 
     log.info("Mapping stripped helper vocabulary...")
-    model_strip = map_strip_base(model.vocab)
+    strip_pat_2 = re.compile("_+|-+|/+|\\\\+| +")
+    strip_pat_3a = re.compile("_+")
+    strip_pat_3b = re.compile("[0-9][0-9]+")
+    strip_pat_4a = re.compile("[^A-Za-z0-9#%$\\.,;/]")
+    strip_pat_4b = re.compile("#+")
+    strip_pat_5a = re.compile("[\\.,;/]")
+    strip_pat_5b = re.compile("#+")
+    strip_helpers = [
+        (lambda text: text.lower()),
+        (lambda text: strip_pat_2.sub("_", text)),
+        (lambda text: strip_pat_3b.sub("##", strip_pat_3a.sub("", text))),
+        (lambda text: strip_pat_4b.sub("#", strip_pat_4a.sub("", text))),
+        (lambda text: strip_pat_5b.sub("#", strip_pat_5a.sub("", text))),
+    ]
+    model_strips = map_strips_base(strip_helpers, model.vocab)
 
     log.info("Mapping words/phrases from {}...".format(parses_json))
     sentences_iter = PDTBParsesCorpus(parses_json, reg_split="-|\\\\/")
-    vocab, missing, total_cnt = map_sent_base(sentences_iter, model.vocab, model_strip, longest=True)
+    vocab, missing, total_cnt = map_sent_base(sentences_iter, model.vocab, strip_helpers=strip_helpers, strip_vocabs=model_strips, longest=True)
     log.info("- mappings: {}, missing: {}, total words: {}".format(len(vocab), len(missing), total_cnt))
 
     log.info("Mapping vocabulary to word2vec vectors...")
