f04610c52fe25ba0220e77a0971d0e62ff0cf672
gw0 [http://gw.tnode.com/] <gw.2014@tnode.com>
2015-03-27 20:56:40 +0100

    Improve profiler for time and memory usage.

--- ex01/common.py
+++ ex01/common.py
@@ -33,20 +33,37 @@ class PaddingFilter(logging.Filter):
 
 ### Debug helpers
 
+class Profiler(object):
+    """Helper for monitoring time and memory usage."""
+
+    def __init__(self, log):
+        self.log = log
+        self.start()
+
+    def start(self):
+        self.time_0 = time.time()
+        self.mem_0 = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    
+    def stop(self):
+        self.time_1 = time.time()
+        self.mem_1 = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        self.print_usage()
+
+    def print_usage(self):
+        self.log.error("(time {:.3f}s, memory {:+.1f}MB, total {:.3f}GB)".format(self.time_1 - self.time_0, (self.mem_1 - self.mem_0) / 1024.0, self.mem_1 / 1024.0 / 1024.0))
+
+
 def profile(func, log=None):
     """Decorator for monitoring time and memory usage."""
 
     if log is None:
         log = logging.getLogger(func.__module__)
+    profiler = Profiler(log)
 
     def wrap(*args, **kwargs):
-        time_0 = time.time()
-        mem_0 = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        profiler.start()
         res = func(*args, **kwargs)
-        time_1 = time.time()
-        mem_1 = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-
-        log.error("(time {:.3f}s, memory {:+.1f}MB, total {:.3f}GB)".format(time_1 - time_0, (mem_1 - mem_0) / 1024.0, mem_1 / 1024.0 / 1024.0))
+        profiler.stop()
         return res
 
     return wrap
@@ -54,7 +71,7 @@ def profile(func, log=None):
 
 ### Defaults
 
-log_modules = ['ex01', 'ex02', 'ex03', 'ex04']
+log_modules = ['ex01', 'ex02']
 logging.basicConfig(format="[%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M", level=logging.DEBUG)
 for handler in logging.root.handlers:
     handler.addFilter(PaddingFilter(log_modules))
